68bdcd04739e717a2aab8b45cf8216bae33f3fe9
tuda2008 <1756991323@qq.com>
2018-08-07 13:38:01 +0800

    refactor

--- lib/generators/redactor2/templates/active_record/carrierwave/redactor2/image.rb
+++ lib/generators/redactor2/templates/active_record/carrierwave/redactor2/image.rb
@@ -4,4 +4,13 @@ class Redactor2Rails::Image < Redactor2Rails::Asset
   def url_content
     url(:content)
   end
+
+  def as_json(opts = {})
+    {
+      thumb: self.url(:thumb),
+      url: self.url_content,
+      id: self.id.to_s,
+      title: self.data_file_name.split('.').first
+    }
+  end
 end
